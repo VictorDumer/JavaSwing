fe7eedf9e6c81235f4e3a8dbfcec2e9d59bc8122
VictorDumer <Victorgabrieldummersouza@gmail.com>
2025-05-20 23:09:30 -0300

    (w.i.p) first update, left titles + buttons

--- Tela.java
+++ Tela.java
@@ -1,14 +1,13 @@
-import java.awt.Color;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class Tela extends JFrame{
     private JPanel tela;
-    private JLabel Tipo,Nome,Rg,Email,Pais,Municipio,Logadouro,Numero,Bairro,Expedor,Cep,Uf,Telefone,Situacao,Fornecedor,Titulo;
+    private JLabel Tipo,Nome,Rg,Email,Pais,Municipio,Logadouro,Numero,Bairro,Expedor,Cep,Uf,Telefone,Situacao,Fornecedor,Titulo,Cpf,Complemento;
     private JTextField envTipo,envNome,envCpf,envFornecedor,envRg,envExpedor,envEmail,envCep,envPais,envUf,envUF2,envMunicipio,envLogadouro,envNumero,envComplemento,envBairro,envTelefone;
     private JRadioButton radAtivo,radDesativo;
     private ButtonGroup grupoRadio;
-    private JComboBox<String> comboBox = new JComboBox<>();
+    private JComboBox<String> comboBox;
     private JButton Confirmar,Excluir,Limpar;
     private JCheckBox checkFornecedor;
 
@@ -20,28 +19,130 @@ public class Tela extends JFrame{
     public void Janela(){
             
             tela = new JPanel();
+            tela.setLayout(null);
             //Título
+
+
             Titulo= new JLabel("Cadastro de Pessoa");
-            Titulo.setBounds(500, 500, 300, 300);
-            Titulo.setForeground(Color.ORANGE);
+            Titulo.setBounds(30, 30, 600,50);
+            Titulo.setFont(new Font("Arial", Font.BOLD, 60));
+            Titulo.setOpaque(true);
+            Titulo.setForeground(Color.orange);
+            
+        //!------------ parte dos textos---------------
         	//Nomes
             Tipo = new JLabel("Tipo");
+            Tipo.setBounds(30,110,170,30);
+            Tipo.setBorder(BorderFactory.createLineBorder(Color.black));
+            Tipo.setBackground(Color.WHITE);
+            Tipo.setVerticalAlignment(JLabel.CENTER);
+            Tipo.setHorizontalAlignment(JLabel.CENTER);
+
+
             Nome = new JLabel("Nome");
-            Rg = new JLabel("RG/Incrição Estadual");
+            Nome.setBounds(30,150,170,30);
+            Nome.setBorder(BorderFactory.createLineBorder(Color.black));
+            Nome.setVerticalAlignment(JLabel.CENTER);
+            Nome.setHorizontalAlignment(JLabel.CENTER);
+
+            Cpf = new JLabel("Cpf/Cnpj..");
+            Cpf.setBounds(30,150,170,30);
+            Cpf.setBorder(BorderFactory.createLineBorder(Color.black));
+            Cpf.setVerticalAlignment(JLabel.CENTER);
+            Cpf.setHorizontalAlignment(JLabel.CENTER);
+
+            Rg = new JLabel("RG / Incrição Estadual");
+            Rg.setBounds(30,190,170,30);
+            Rg.setBorder(BorderFactory.createLineBorder(Color.black));
+            Rg.setVerticalAlignment(JLabel.CENTER);
+            Rg.setHorizontalAlignment(JLabel.CENTER);
+
             Email = new JLabel("Email");
+            Email.setBounds(30,230,170,30);
+            Email.setBorder(BorderFactory.createLineBorder(Color.black));
+            Email.setVerticalAlignment(JLabel.CENTER);
+            Email.setHorizontalAlignment(JLabel.CENTER);
+            
             Pais = new JLabel("Pais");
+            Pais.setBounds(30,270,170,30);
+            Pais.setBorder(BorderFactory.createLineBorder(Color.black));
+            Pais.setVerticalAlignment(JLabel.CENTER);
+            Pais.setHorizontalAlignment(JLabel.CENTER);
+            
             Municipio = new JLabel("Município ...");
+            Municipio.setBounds(30,310,170,30);
+            Municipio.setBorder(BorderFactory.createLineBorder(Color.black));
+            Municipio.setVerticalAlignment(JLabel.CENTER);
+            Municipio.setHorizontalAlignment(JLabel.CENTER);
+            
             Logadouro = new JLabel("Logadouro");
+            Logadouro.setBounds(30,350,170,30);
+            Logadouro.setBorder(BorderFactory.createLineBorder(Color.black));
+            Logadouro.setVerticalAlignment(JLabel.CENTER);
+            Logadouro.setHorizontalAlignment(JLabel.CENTER);
+
+            
             Numero = new JLabel("Numero");
+            Numero.setBounds(30,390,170,30);
+            Numero.setBorder(BorderFactory.createLineBorder(Color.black));
+            Numero.setVerticalAlignment(JLabel.CENTER);
+            Numero.setHorizontalAlignment(JLabel.CENTER);
+            
             Bairro = new JLabel("Bairro");
+            Bairro.setBounds(30,430,170,30);
+            Bairro.setBorder(BorderFactory.createLineBorder(Color.black));
+            Bairro.setVerticalAlignment(JLabel.CENTER);
+            Bairro.setHorizontalAlignment(JLabel.CENTER);
+
             Expedor = new JLabel("Orgão Expedor");
+            Expedor.setBounds(30,0,170,30);
+            Expedor.setBorder(BorderFactory.createLineBorder(Color.black));
+            Expedor.setVerticalAlignment(JLabel.CENTER);
+            Expedor.setHorizontalAlignment(JLabel.CENTER);
+
             Cep = new JLabel("CEP");
+            Cep.setBounds(30,0,170,30);
+            Cep.setBorder(BorderFactory.createLineBorder(Color.black));
+            Cep.setVerticalAlignment(JLabel.CENTER);
+            Cep.setHorizontalAlignment(JLabel.CENTER);
+
+            Complemento = new JLabel("Complemento");
+            Complemento.setBounds(30,0,170,30);
+            Complemento.setBorder(BorderFactory.createLineBorder(Color.black));
+            Complemento.setVerticalAlignment(JLabel.CENTER);
+            Complemento.setHorizontalAlignment(JLabel.CENTER);
+            
             Uf = new JLabel("UF ...");
+            Uf.setBounds(30,0,170,30);
+            Uf.setBorder(BorderFactory.createLineBorder(Color.black));
+            Uf.setVerticalAlignment(JLabel.CENTER);
+            Uf.setHorizontalAlignment(JLabel.CENTER);
+
             Telefone = new JLabel("Telefone");
+            Telefone.setBounds(30,0,170,30);
+            Telefone.setBorder(BorderFactory.createLineBorder(Color.black));
+            Telefone.setVerticalAlignment(JLabel.CENTER);
+            Telefone.setHorizontalAlignment(JLabel.CENTER);
+
             Situacao = new JLabel("Situação");
+            Situacao.setBounds(30,0,170,30);
+            Situacao.setBorder(BorderFactory.createLineBorder(Color.black));
+            Situacao.setVerticalAlignment(JLabel.CENTER);
+            Situacao.setHorizontalAlignment(JLabel.CENTER);
+
+            Fornecedor = new JLabel("Fornecedor");
+            Fornecedor.setBounds(30,0,170,30);
+            Fornecedor.setBorder(BorderFactory.createLineBorder(Color.black));
+            Fornecedor.setVerticalAlignment(JLabel.CENTER);
+            Fornecedor.setHorizontalAlignment(JLabel.CENTER);
+        //!------------ parte dos textos ---------------
+
+
+            
 
     
-        //Entrada de dados
+        //?------------ Entrada de dados ---------------
+
             envTipo = new JTextField(20);
             envNome = new JTextField(100);
             envRg = new JTextField(20);
@@ -53,13 +154,20 @@ public class Tela extends JFrame{
             envBairro = new JTextField(20);
             envExpedor = new JTextField(20);
             envCep = new JTextField(20);
+            envCpf = new JTextField(20);
             envUf = new JTextField(20);
+            envFornecedor = new JTextField(20);
+            envUF2 = new JTextField(20);
+            envComplemento = new JTextField(20);
             envTelefone = new JTextField(20);
+        //?------------ Entrada de dados ---------------
     
         // Combo box
-            String[] opcoes = {"Física","Jurídica"};
+            String[] opcoes = {"Física", "Jurídica"};
             comboBox = new JComboBox<>(opcoes);
-    
+            comboBox.setBounds(170,30,170,30);
+        
+            
         //Radio Group
             radAtivo = new JRadioButton("Ativo");
             radDesativo = new JRadioButton("Desativo");
@@ -71,17 +179,33 @@ public class Tela extends JFrame{
         // Botoes
         Confirmar = new JButton("Confirmar");
             Confirmar.addActionListener(e -> System.out.println("Enviado dados"));
+            Confirmar.setBounds(30,490,140,30);
+            Confirmar.setBorder(BorderFactory.createLineBorder(Color.gray));
+            Confirmar.setVerticalAlignment(JLabel.CENTER);
+            Confirmar.setHorizontalAlignment(JLabel.CENTER);
+            Confirmar.setBackground(Color.orange);
+            Confirmar.setForeground(Color.white);
+
             
             Excluir = new JButton("Excluir");
-            
+            Excluir.setBounds(180,490,140,30);
+            Excluir.setBorder(BorderFactory.createLineBorder(Color.gray));
+            Excluir.setVerticalAlignment(JLabel.CENTER);
+            Excluir.setHorizontalAlignment(JLabel.CENTER);
+            Excluir.setBackground(Color.orange);
+            Excluir.setForeground(Color.white);
             Excluir.addActionListener(e -> System.out.println("Excluindo dados"));
             
             Limpar = new JButton("Limpar");
-            
+            Limpar.setBounds(330,490,140,30);
+            Limpar.setBorder(BorderFactory.createLineBorder(Color.gray));
+            Limpar.setVerticalAlignment(JLabel.CENTER);
+            Limpar.setHorizontalAlignment(JLabel.CENTER);
+            Limpar.setBackground(Color.gray);
             Limpar.addActionListener(e -> System.out.println("Campos limpos"));
     
         // checkbox
-        checkFornecedor = new JCheckBox("Sou fornecedor");
+        checkFornecedor = new JCheckBox();
         tela.add(Tipo);
         tela.add(Nome);
         tela.add(Rg);
@@ -96,7 +220,6 @@ public class Tela extends JFrame{
         tela.add(Uf);
         tela.add(Telefone);
         tela.add(Situacao);
-
         tela.add(envTipo);
         tela.add(envNome);
         tela.add(envRg);
@@ -118,16 +241,15 @@ public class Tela extends JFrame{
         tela.add(radAtivo);
         tela.add(radDesativo);
         tela.add(comboBox);
+        tela.repaint();
         
-        this.setLocationRelativeTo(null);
+        
         this.setContentPane(tela);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(1000,1000);
+        this.setSize(1000,600);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Tela();
-    }
+    
 }
